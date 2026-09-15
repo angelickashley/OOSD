b6dc26b6b91caa5673574764ebca6cd0a91ad4f8
Angelic Kyle Ashley <akaangelic.work@gmail.com>
2026-09-15 09:09:08 +0100

    Delete java/lab1 directory

--- java/lab1/lab1g1java/src/lab1q1.java
+++
@@ -1,8 +0,0 @@
-public class lab1q1 {
-    public static void main (String[] args){
-        ThermTest.main();
-        Thermometer thermB = new Thermometer(10.0);
-        double tempB = thermB.getCelsius();
-        System.out.println("Temp. of Thermometer B is " + tempB);
-    }
-}
